742e2db504822a515f0e7dec99181422c83e8e83
YvanLi <stfujnkklyf@gmail.com>
2023-10-15 13:37:18 +0800

    chore: support setting IP when signing

--- tools/sign.cmd
+++ tools/sign.cmd
@@ -1,6 +1,9 @@
 @echo off
 
 if "%~1" == "" goto end1
+set ip=127.0.0.1
+if "%~2" neq "" set ip=%~2
+
 rmdir /Q/S %1>nul 2>nul
 md %1\keys
 md %1\certs
@@ -9,10 +12,11 @@ openssl req -new -key %1/keys/%1.key -out %1/certs/%1.csr -subj "/C=CN/O=pzrp/OU
 set extfile=%1/certs/%1_cert_extensions
 echo basicConstraints=CA:FALSE > %extfile%
 echo keyUsage=nonRepudiation, digitalSignature, keyEncipherment >> %extfile%
-echo subjectAltName=DNS.1:localhost, IP.1:127.0.0.1 >> %extfile%
+echo subjectAltName=DNS.1:localhost, IP.1:%ip% >> %extfile%
 openssl x509 -req -days 365 -sha256 -extfile %extfile% -CA ca/certs/ca.crt -CAkey ca/keys/ca.key -in %1/certs/%1.csr -out %1/certs/%1.crt -CAcreateserial
+copy ca/certs/ca.crt %1/ca.crt
 goto:eof
 
 :end1
-echo "certificate name needs to be specified"
+echo certificate name needs to be specified
 goto:eof
--- tools/sign.sh
+++ tools/sign.sh
@@ -4,6 +4,10 @@ if [ "$1" = "" ];then
   echo certificate name needs to be specified
   exit
 fi
+ip="127.0.0.1"
+if [ "$2" != "" ];then
+  ip="$2"
+fi
 
 rm -rf $1/*
 mkdir -p $1/certs
@@ -13,5 +17,6 @@ openssl req -new -key $1/keys/$1.key -out $1/certs/$1.csr -subj "/C=CN/O=pzrp/OU
 extfile="$1/certs/${1}_cert_extensions"
 echo "basicConstraints=CA:FALSE" > $extfile
 echo "keyUsage=nonRepudiation, digitalSignature, keyEncipherment" >> $extfile
-echo "subjectAltName=DNS.1:localhost, IP.1:127.0.0.1" >> $extfile
+echo "subjectAltName=DNS.1:localhost, IP.1:$ip" >> $extfile
 openssl x509 -req -days 365 -sha256 -extfile $extfile -CA ca/certs/ca.crt -CAkey ca/keys/ca.key -in $1/certs/$1.csr -out $1/certs/$1.crt -CAcreateserial
+cp ca/certs/ca.crt $1/ca.crt
