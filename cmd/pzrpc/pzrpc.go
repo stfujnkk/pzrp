46a01d2532deaf1030061a2fe5458b8c01f02641
stfujnkk <52458236+stfujnkk@users.noreply.github.com>
2023-11-15 11:47:12 +0800

    Fix/using err abnormal packet (#2)
    * fix: use ErrAbnormalPacket
    
    Check for abnormal packages
    
    * test(msg): move `msg_test.go`
    
    * feat: add version
    
    ---------
    
    Co-authored-by: YvanLi <stfujnkklyf@gmail.com>

--- cmd/pzrpc/pzrpc.go
+++ cmd/pzrpc/pzrpc.go
@@ -3,13 +3,20 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"pzrp/client"
+	"pzrp/pkg/common"
 	"pzrp/pkg/config"
 )
 
 func main() {
 	configPath := flag.String("config", "pzrpc.json", "configuration file path")
+	showVersion := flag.Bool("version", false, "display version number")
 	flag.Parse()
+	if *showVersion {
+		fmt.Println(common.VERSION)
+		return
+	}
 	conf, err := config.LoadClientConfig(*configPath)
 	if err != nil {
 		panic(err)
