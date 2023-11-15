5510b5b557d6312dd1a1a54ae12ffe4eb6d7ac28
YvanLi <stfujnkklyf@gmail.com>
2023-11-15 11:45:34 +0800

    feat: add version

--- cmd/pzrps/pzrps.go
+++ cmd/pzrps/pzrps.go
@@ -3,13 +3,20 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"pzrp/pkg/common"
 	"pzrp/pkg/config"
 	"pzrp/server"
 )
 
 func main() {
 	configPath := flag.String("config", "pzrps.json", "configuration file path")
+	showVersion := flag.Bool("version", false, "display version number")
 	flag.Parse()
+	if *showVersion {
+		fmt.Println(common.VERSION)
+		return
+	}
 	conf, err := config.LoadServerConfig(*configPath)
 	if err != nil {
 		panic(err)
