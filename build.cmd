41691d4947813905602e9666913e62c3d44f32ce
YvanLi <stfujnkklyf@gmail.com>
2024-02-23 17:55:45 +0800

    build: add `trimpath` parameter

--- build.cmd
+++ build.cmd
@@ -7,18 +7,18 @@ set CGO_ENABLED=0
 set GOOS=linux
 set GOARCH=amd64
 
-go build -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\linux_amd64\ cmd\pzrpc\pzrpc.go
-go build -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\linux_amd64\ cmd\pzrps\pzrps.go
+go build -trimpath -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\linux_amd64\ cmd\pzrpc\pzrpc.go
+go build -trimpath -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\linux_amd64\ cmd\pzrps\pzrps.go
 
 set CGO_ENABLED=0
 set GOOS=windows
 set GOARCH=amd64
-go build -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\windows_amd64\ cmd\pzrpc\pzrpc.go
-go build -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\windows_amd64\ cmd\pzrps\pzrps.go
+go build -trimpath -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\windows_amd64\ cmd\pzrpc\pzrpc.go
+go build -trimpath -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\windows_amd64\ cmd\pzrps\pzrps.go
 
 
 set CGO_ENABLED=0
 set GOOS=darwin
 set GOARCH=amd64
-go build -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\darwin_amd64\ cmd\pzrpc\pzrpc.go
-go build -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\darwin_amd64\ cmd\pzrps\pzrps.go
+go build -trimpath -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\darwin_amd64\ cmd\pzrpc\pzrpc.go
+go build -trimpath -ldflags "-s -w -X 'main.VERSION=%version%'" -o bin\darwin_amd64\ cmd\pzrps\pzrps.go
